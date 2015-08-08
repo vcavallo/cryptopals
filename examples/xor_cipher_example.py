37ca2bc86bcdabf21306d97a657bf0fd28e6bfbc
vinney cavallo <vcavallo@gmail.com>
2015-08-08 19:10:16 -0400

    Add examples folder for other peoples' code

---
+++ examples/xor_cipher_example.py
@@ -0,0 +1,29 @@
+#! /usr/local/bin/python
+
+# from https://en.wikipedia.org/wiki/XOR_cipher
+
+from os import urandom
+
+def genkey(length):
+  """Generate key"""
+  return urandom(length)
+
+def xor_strings(s,t):
+  """xor two strings together"""
+  return "".join(chr(ord(a)^ord(b)) for a,b in zip(s,t))
+
+message = 'This is a secret message'
+print 'message:', message
+
+key = genkey(len(message))
+print 'key:', key
+
+cipherText = xor_strings(message, key)
+print 'cipherText:', cipherText
+print 'decrypted:', xor_strings(cipherText,key)
+
+# verify
+if xor_strings(cipherText, key) == message:
+  print 'Unit test passed'
+else:
+  print 'Unit test failed'
